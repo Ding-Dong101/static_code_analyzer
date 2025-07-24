5b29891efbffcec9ebe569ead0dcc4ad3f673db8
Uriah <99614971+Ding-Dong101@users.noreply.github.com>
2025-07-24 15:14:37 +0000

    Add files via upload
    main.cpp started

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-  cout << "Welcome \n";
+  // variables
+  string file_name;
+  string line;
+  string logo = "logo.txt";
+  // this gets the texts in the file
+  ifstream file(file_name);
+  ifstream logoFile(logo);
+
+  while (getline(logoFile, line)) {
+    cout << line << endl;
+  }
+  for (int i; i <= 3; ++i) {
+    cout << endl;
+  }
+  cout << "Enter the name of the file (the file should be in the same "
+          "directory): \n";
+  cout << "[+] ";
+  cin >> file_name;
+
+  // this is a check or validation if the file exists
+  if (!file) {
+    cout << "[+] There was an error getting the file name please check "
+            "agian... \n";
+  } else {
+    for (int i; i <= 2; ++i) {
+      cout << endl;
+    }
+
+    //   loops through the "file" to generate the text inside
+    while (getline(file, line)) {
+      cout << line << endl;
+    }
+
+    for (int i; i <= 5; ++i) {
+      cout << endl;
+    }
+  }
+  cout << "[+] Program terminated..." << endl;
   return 0;
 }
